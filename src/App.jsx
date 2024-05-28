e48fd9b47ba96c63e0f9bd3d665d327eddce978a
grvsanthosh <grvsanthosh@gmail.com>
2024-05-28 23:58:01 +0530

    Landing page project

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Navbar from "./components/Navbar";
+import CalltoAction from "./components/CalltoAction";
+import Footer from "./components/Footer";
+import Icons from "./components/Icons";
+import Imageshow from "./components/Imageshow";
+import Masterhead from "./components/Masterhead";
+import Testimonials from "./components/Testimonials";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Masterhead />
+      <Icons />
+      <Imageshow />
+      <Testimonials />
+      <CalltoAction />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
